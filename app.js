34bd8b65ac063f64ca6231f6dd98a541c4538598
niccentury <cjdcec@gmail.com>
2018-08-19 17:34:54 -0700

    finished sales page

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-'use strict'
+'use strict';
 
 console.log('js loaded');
 
 var local = ['<th>First and Pike</th>', '<th>SeaTac Airport</th>', '<th>Seattle Center</th>', '<th>Capitol Hill', '<th>Alki</th>'];
-var hours = ["6am: ", "7am: ", "8am: ", "9am: ", "10am: ", "11am: ", "12pm: ", "1pm: ", "2pm: ", "3pm: ", "4pm: ", "5pm: ", "6pm: ", "7pm: ", "8pm: "];
+var hours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: '];
 
 //Funtion to generate a random nuber
 function getRandomIntInclusive() {
@@ -21,7 +21,7 @@ function getAvgCookPerHour() {
   return this.randPerHour;
 
 }
-//The maximum is inclusive and the minimum is inclusive 
+//The maximum is inclusive and the minimum is inclusive
 
 function Locations(location, minCust, maxCust, avgCookSale) {
   this.location = location;
@@ -42,7 +42,7 @@ var capitolHill = new Locations(local[3], 20, 38, 2.3);
 var alki = new Locations(local[4], 2, 16, 4.6);
 
 
-var objectArray = [firstAndPike, seaTacAirport, seattleCenter, capitolHill, alki]
+var objectArray = [firstAndPike, seaTacAirport, seattleCenter, capitolHill, alki];
 
 //Function to fill in custructor with actuals
 function fillTable(storeData) {
@@ -59,33 +59,33 @@ function fillTable(storeData) {
   }
   var dailyTotal = document.createElement('td');
   dailyTotal.textContent = totalCooks;
- tableData.appendChild(dailyTotal);
+  tableData.appendChild(dailyTotal);
 }
 
-var dailyTotalArray = []
-var hourlyTotalArray = []
+var dailyTotalArray = [];
+var hourlyTotalArray = [];
 function StoreTotal () {
   var hoursIndex = 0;
   hourlyTotalArray = [];
   for (var i = 0; i <= hours.length; i++) {
-      if (i < hours.length) {
-          var hourlyTotal = 0;
-          for ( var x = 0 ; x < objectArray.length; x++) {
-              hourlyTotal += objectArray[x].randPerHour[hoursIndex];
-          }
-          hourlyTotalArray.push(hourlyTotal); 
-          
+    if (i < hours.length) {
+      var hourlyTotal = 0;
+      for ( var x = 0 ; x < objectArray.length; x++) {
+        hourlyTotal += objectArray[x].randPerHour[hoursIndex];
+      }
+      hourlyTotalArray.push(hourlyTotal);
 
-      }else{
-           var allStoresDailyTotal = 0;
-            for (var x = 0; x < dailyTotalArray.length; x++) {
-              allStoresDailyTotal += dailyTotalArray[x];
-            }
-          
-          //htmlTableRow.appendChild(allStoresDailyTotal);
+
+    }else{
+      var allStoresDailyTotal = 0;
+      for (var x = 0; x < dailyTotalArray.length; x++) {
+        allStoresDailyTotal += dailyTotalArray[x];
       }
 
-      hoursIndex++;
+      //htmlTableRow.appendChild(allStoresDailyTotal);
+    }
+
+    hoursIndex++;
   }
 
 }
@@ -101,33 +101,33 @@ function createFooter(){
   for (var i = 0; i <= hourlyTotalArray.length; i++){
     var totalTd = document.createElement('td');
     if (i === hourlyTotalArray.length) {
-      totalTd.textContent = hourlyTotalArray.reduce((a, b) => a + b, 0)
+      totalTd.textContent = hourlyTotalArray.reduce((a, b) => a + b, 0);
       totalsInRow.appendChild(totalTd);
       break;
-      }
+    }
     totalTd.textContent = hourlyTotalArray[i];
     totalsInRow.appendChild(totalTd);
-    }
+  }
 }
 
 function newFooter(hasFooter = false){
-  if (hasFooter ==  false) {
+  if (hasFooter == false) {
     createFooter();
   }
   else if (hasFooter == true) {
     document.getElementById('cookieTable').deleteTFoot();
     var table = document.getElementById('cookieTable');
-    var footer = table.createTFoot()
+    var footer = table.createTFoot();
     createFooter();
   }
   else {
-    console.log("only accepts bool as argument");
+    console.log('only accepts bool as argument');
   }
 }
-  // if i = hourlyTotalArray.length}{
-  //   hourlyTotalArray.reduce((a, b) => a + b, 0)
+// if i = hourlyTotalArray.length}{
+//   hourlyTotalArray.reduce((a, b) => a + b, 0)
 
-  
+
 
 //Call vars necessary to execute functions
 
